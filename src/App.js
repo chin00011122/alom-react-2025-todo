de0556cedf7b0580535a497106538c323a317996
chin00011122 <chin0928@naver.com>
2025-05-11 01:14:12 +0900

    진짜 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import {Box} from "./components/styles";
+import { Box } from "./components/styles";
 
 /**
  * 과제1-2: 이곳에 assignments 폴더 내 작성한 코드를 불러오는 코드를 작성해주세요.
@@ -14,27 +14,27 @@ import Navigation from "./Navigation.js";
 function App() {
   return (
     <Router>
-    
+
       <Routes>
         {/* 과제1-2: 이곳에 과제1-1에서 작성한 코드를 불러오는 코드를 작성해주세요.
             ex) <Route path="/woojin" element={<Woojin />} />
         */}
+        <Route path="/" element={<div>TodoApp.js와 example.js를 확인해주세요.</div>} />
 
-       <Route path="/" element={<div>TodoApp.js와 example.js를 확인해주세요.</div>} />
-          
         {/* Example과 TodoApp을 개별 경로로 분리 */}
 
-      <Route path="/example" element={<Box><Example /></Box>} />
-      <Route path="/TodoApp" element={<Box><TodoApp /></Box>} />
-      
+        <Route path="/example" element={<Box><Example /></Box>} />
+        <Route path="/TodoApp" element={<Box><TodoApp /></Box>} />
+
 
       </Routes>
       <nav>
-      <Link to="/example">Example</Link> | 
-      <Link to="/TodoApp">TodoApp</Link>
+        <Link to="/example">Example</Link> |
+        <Link to="/TodoApp">TodoApp</Link>
       </nav>
-      </Router>
+    </Router>
   );
+
 }
 
 export default App;
